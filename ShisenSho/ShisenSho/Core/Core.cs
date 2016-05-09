47dbc7e58e41e98c91746f5a05e49eb75a7a0f42
MascioSpam <masciospam@openmailbox.org>
2016-05-09 17:34:18 +0200

    [Core] first commit

---
+++ ShisenSho/ShisenSho/Core/Core.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Gtk;
+
+namespace ShisenSho
+{
+	public class Core
+	{
+		public Core ()
+		{
+		}
+
+		public int getBoardHeight ()
+		{
+			return 6;
+		}
+
+		public int getBoardWidth ()
+		{
+			return 12;
+		}
+	}
+}
+
