20c9d2b2e27592f0eab333a5e4c2dcf733246ac3
MascioSpam <masciospam@openmailbox.org>
2016-05-12 11:31:52 +0200

    [Core] implemented NO_TILE_TYPE and scramble_board function

--- ShisenSho/ShisenSho/Core/Core.cs
+++ ShisenSho/ShisenSho/Core/Core.cs
@@ -5,11 +5,12 @@ namespace ShisenSho
 {
 	public class Core
 	{
+		public const int NO_TILE_TYPE = 0;
 		private int height;
 		private int width;
 		private int brickNumber;	// Number of different type of bricks
 		private int [,] board;
-		private int brickCount;	// Number of bricks whit value != 0
+		private int brickCount;	// Number of bricks whit value != NO_TILE_TYPE
 		private Random rnd;
 
 		public Core (int height, int width, int brickNumber)
@@ -56,11 +57,32 @@ namespace ShisenSho
 		{
 			bool res = false;
 			if (board [x1, y1] == board [x2, y2]) {
-				board [x1, y1] = board [x2, y2] = 0;
+				board [x1, y1] = board [x2, y2] = NO_TILE_TYPE;
 				res = true;
 				brickCount -= 2;
 			}
 			return res;
+		}
+
+		public void scramble_board ()
+		{
+			int temp_x = 0;
+			int temp_y = 0;
+			int temp_type = NO_TILE_TYPE;
+
+			// Scrambling board's tiles
+			for (int i = 1; i <= this.width; i++)
+				for (int j = 1; j <= this.height; j++)
+					if (this.board [i,j] > NO_TILE_TYPE)
+					if (temp_type == NO_TILE_TYPE) {
+						temp_x = i;
+						temp_y = j;
+						temp_type = this.board [i,j];
+					} else {
+						this.board [temp_x,temp_y] = this.board [i,j];
+						this.board [i,j] = temp_type;
+						temp_type = NO_TILE_TYPE;
+					}
 		}
 	}
 }
