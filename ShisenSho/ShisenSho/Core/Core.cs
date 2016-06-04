35726d4214c30375b32baa00677ea406fbcb3830
DodoIta <davide.cor94@gmail.com>
2016-06-04 16:22:33 +0200

    [Core] Finished method pathViability

--- ShisenSho/ShisenSho/Core/Core.cs
+++ ShisenSho/ShisenSho/Core/Core.cs
@@ -175,12 +175,12 @@ namespace ShisenSho
 			else if (x1 > x2 && y1 < y2) {	// Second tile is bottom-left
 				if (board [x1 - 1, y1] == 0 && checkPath (x1, y1, x2, y2, 9))	// Try starting from the left
 					return true;
-				/*else if (board [x1, y1 - 1] == 0 && checkPath (x1, y1, x2, y2, 8))	// Try starting from the top
+				else if (board [x1, y1 - 1] == 0 && checkPath (x1, y1, x2, y2, 5))	// Try starting from the top
 					return true;
-				else if (board [x1 + 1, y1] == 0 && checkPath (x1, y1, x2, y2, 4))	// Try starting from the right
+				else if (board [x1 + 1, y1] == 0 && checkPath (x1, y1, x2, y2, 11))	// Try starting from the right
 					return true;
-				else if (board [x1, y1 + 1] == 0 && checkPath (x1, y1, x2, y2, 6))	// Try starting from the bottom
-					return true;*/
+				else if (board [x1, y1 + 1] == 0 && checkPath (x1, y1, x2, y2, 10))	// Try starting from the bottom
+					return true;
 				else
 					return false;
 			}
@@ -411,7 +411,69 @@ namespace ShisenSho
 						else
 							return false;
 					}
-				}		
+				}
+			case 10:	// First tile top-right; all paths starting from the bottom
+				// First try the shortest paths
+				for (j = y1; (j != y2 && board [x1, j + 1] == 0 && j <= this.height + 1); j++)	// Go down
+					;
+				if (j == y2) {	// Try going left
+					for (i = x1 - 1; (i != x2 && board [i, j] == 0 && i >= 0); i--)
+						;
+					if (i == x2)
+						return true;
+					else {	// Try down-left-up
+						j = j + 1;	// Go down
+						do {	// Then go left
+							for (i = x1 - 1; (i != x2 && board [i, j] == 0 && i >= 0); i--)
+								;
+							j++;	// Go down until you can't go left
+						} while (i != x2 && j <= this.height + 1 && board [x1, j] == 0);
+						if (i == x2) {	// Go up (last turn)
+							for (j = j - 1; (j > y2 && board [i, j] == 0); j--)
+								;
+							if (j == y2)
+								return true;	// End of case three (down-left-up)
+							else
+								return false;
+						} else
+							return false;
+					}	// End down-left-up
+				}	// End if (j == y2)
+				else {
+					j = y1 + 1;	// Go down
+					do {	// Then go left
+						for (i = x1; (i != x2 && board [i - 1, j] == 0 && i >= 0); i--)
+							;
+						j++;
+					} while (i != x2 && j <= this.height + 1 && board [x1, j] == 0);
+					if (i != x2)
+						return false;
+					else {	// Go down (final step)
+						for (; (j != y2 && board [i, j] == 0 && j <= this.height + 1); j++)
+							;
+						if (j == y2)
+							return true;
+						else
+							return false;
+					}
+				}
+			case 11:	// First tile top-right; path starting from the right
+				i = x1 + 1;
+				do {	// Then go down
+					for (j = y1; (j != y2 && board [i, j + 1] == 0 && j <= this.height + 1); j++)
+						;
+					i++;
+				} while (j != y2 && i <= this.width + 1 && board [i, y1] == 0);
+				if (j == y2) {	// Go left (last turn)
+					for (i = i - 1; (i > x2 && board [i, j] == 0); i--)
+						;
+					if (i == x2)
+						return true;	// End of case right-down-left
+					else
+						return false;
+				}
+				else 
+					return false;
 			default:
 				// To be implemented
 				return true;
